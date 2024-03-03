761722ac830bf972ccf5e8ffab5f1b462d2b3e8c
Chengheng Li <li471115381@gmail.com>
2024-03-03 12:30:13 -0800

    add backend logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MapComponent from './MapComponent'; // Adjust the path according to your file structure
+import { fetchObituaries, postObituary } from './data.js';
 
 function App() {
+  // Initialize the obituaries state as an empty array
+  const [obituaries, setObituaries] = useState([]);
+
+  // Fetch obituaries when the component mounts
+  useEffect(() => {
+    const displayObituaries = async () => {
+      const fetchedObituaries = await fetchObituaries();
+      setObituaries(fetchedObituaries); // Store the fetched obituaries in the state
+      console.log(fetchedObituaries);
+
+
+    };
+
+    displayObituaries();
+  }, []); // The empty array ensures this effect runs only once after the initial render
+
   return (
     <div className="App">
       <MapComponent />
+      {}
     </div>
   );
 }
