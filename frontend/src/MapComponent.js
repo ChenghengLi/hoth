c19780058eed8720d6aa91fceb3fd14802bf8d5f
Chengheng Li <li471115381@gmail.com>
2024-03-03 11:20:57 -0800

    add map

---
+++ frontend/src/MapComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const Map = () => {
+  const position = [51.505, -0.09]; // Replace with your desired coordinates
+  const zoom = 13; // Replace with your desired zoom level
+
+  return (
+    <MapContainer center={position} zoom={zoom} style={{ height: '100vh', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
