c86144b30d7e5795c9e96fca552dfc47d59ef43a
liz <liyulg0@gmail.com>
2024-03-03 17:24:25 -0800

    fixed some bugs

---
+++ frontend/src/componentes/video.js
@@ -0,0 +1,10 @@
+import { HMSPrebuilt } from '@100mslive/roomkit-react';
+
+const videoScreen = () => {  
+    return (
+        <div style={{ height: "100vh" }}>
+        <HMSPrebuilt roomCode="wez-twwo-uvw" />
+      </div>)
+    };
+
+export default videoScreen
